c624e47d48380db0f5e2ffbc7b50b49b8bfbe412
Oleg <olemor@mail.ru>
2018-09-18 11:26:01 +0600

    Домашнее задание Лабораторная 12

--- spec/lab12spec.js
+++ spec/lab12spec.js
@@ -29,3 +29,95 @@ it ("test 3946", function () {
 	var result = task3946(arr2);
 	expect(result).toBeCloseTo(3, 4);
 })
+
+it ("test 4283", function () {
+	var result = task4283(3, [1, 2, 3, 4, 1, 2]);
+	expect(result).toBeCloseTo(1, 4);
+	
+	result = task4283(5, [1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3]);
+	expect(result).toBeCloseTo(0, 4);
+	
+	result = task4283(2, [3, 2, 2, 2, 2, 2]);
+	expect(result).toBeCloseTo(5, 4);
+	
+	result = task4283(1, [4, 4, 4, 4, 4, 4]);
+	expect(result).toBeCloseTo(0, 4);
+})
+
+it ("test 6497", function () {
+	var result = task6497('9 8 9 1 1 5 1 1');
+	expect(result).toEqual('4,6250  3,6250  4,6250  3,3750  3,3750  0,6250  3,3750  3,3750');
+	
+	result = task6497('5 5 5 5 5');
+	expect(result).toEqual('0,0000  0,0000  0,0000  0,0000  0,0000');
+	
+	result = task6497('');
+	expect(result).toEqual('');
+	
+	result = task6497('1000000 1 1');
+	expect(result).toEqual('666666,0000  333333,0000  333333,0000');
+})
+
+it ("в задаче 9774 должен получиться корректный результат", function () {
+	var data1 = [9, 8, 7, 6, 5, 4];
+	task9774(31, data1);
+	expect(data1).toEqual([279, 248, 217, 186, 155, 124]);
+	
+	var data2 = [9, 8, 7, 6, 5, 4];
+	task9774(-3, data2);
+	expect(data2).toEqual([-27, -24, -21, -18, -15, -12]);
+	
+	var data3 = [98, 76, 54];
+	task9774(11, data3);
+	expect(data3).toEqual([1078, 836, 594]);
+	
+	var data4 = [];
+	task9774(11, data4);
+	expect(data4).toEqual([]);
+})
+
+it ("в задаче 9271 должен получиться корректный результат", function () {
+	var data1 = [5, 4, 6, 2, 3, 1];
+	task9271(data1);
+	expect(data1).toEqual([4, 3, 5, 1, 2, 0]);
+	
+	var data2 = [1, 2, 3, 4, 1, 2];
+	task9271(data2);
+	expect(data2).toEqual([0, 1, 2, 3, 0, 1]);
+	
+	var data3 = [3, 4, 1, 2, 3, 4, 1, 2, 3, 1, 2];
+	task9271(data3);
+	expect(data3).toEqual([2, 3, 0, 1, 2, 3, 0, 1, 2, 0, 1]);
+	
+	var data4 = [4, 2, 3, 4, 3, 2];
+	task9271(data4);
+	expect(data4).toEqual([2, 0, 1, 2, 1, 0]);
+	
+	var data5 = [4, 4, 4, 4, 4, 3, 2, 1];
+	task9271(data5);
+	expect(data5).toEqual([3, 3, 3, 3, 3, 2, 1, 0]);
+	
+	var data6 = [4, 4, 4, 4];
+	task9271(data6);
+	expect(data6).toEqual([0, 0, 0, 0]);
+	
+	var data7 = [4, 3];
+	task9271(data7);
+	expect(data7).toEqual([1, 0]);
+})
+
+it ("в задаче 4847 должен получиться корректный результат", function () {
+	expect(task4847(31, 59, 83)).toBeFalsy();
+	expect(task4847(19, 19, 41)).toBeTruthy();
+	expect(task4847(7, 11, 11)).toBeTruthy();
+	expect(task4847(67, -67, 67)).toBeTruthy();
+	expect(task4847(1, 1, 1)).toBeTruthy();
+})
+
+it ("в задаче 6740 должен получиться корректный результат", function () {
+	expect(task6740(2, -3)).toBeFalsy();
+	expect(task6740(2, -1)).toBeTruthy();
+	expect(task6740(4, -1.5)).toBeFalsy();
+	expect(task6740(5, 5)).toBeFalsy();
+	expect(task6740(1.1, -2)).toBeTruthy();
+})
--- src/lab12.js
+++ src/lab12.js
@@ -21,3 +21,51 @@ function task3946 (arr){
 	}
 	return sum/arr.length;
 }
+
+function task4283 (x, arr){
+	var sum = 0;
+	for (var i = 0; i < arr.length; i++){
+		if (arr[i] == x) sum++;
+	}
+	return sum;
+}
+
+function task6497 (x){
+	if (x == '') {return '';}
+	var arr = x.split(' ');
+	var sum = 0;
+	for (var i = 0; i < arr.length; i++){
+		sum += parseInt(arr[i]);
+	}
+	sum = sum / arr.length;
+	for (i = 0; i < arr.length; i++){
+		arr[i] = Math.abs(arr[i] - sum);
+		arr[i] = arr[i].toFixed(4);
+	}
+	arr.toString();
+	return arr.join('  ').replace(/\./g,',');
+}
+
+function task9774 (x, arr){
+	for (var i = 0; i < arr.length; i++){
+		arr[i] *= x;
+	}
+}
+
+function task9271 (arr){
+	var minNum = arr[0];
+	for (var i = 1; i < arr.length; i++){
+		if (arr[i] < minNum) minNum = arr[i];
+	}
+	for (i = 0; i < arr.length; i++){
+		arr[i] -= minNum;
+	}
+}
+
+function task4847 (x1, x2, x3){
+	return (x1 == x2 || x2 == x3 || x3 == x1);
+}
+
+function task6740 (x, y){
+	return ((x >= 1 && x <= 3) && (y >= -2 && y <= -1));
+}
